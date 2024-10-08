c8e000862db01425b82739c7ebb72ec77889a34c
Maicol Code <paucarjhon07@outlook.com>
2024-10-08 14:51:09 -0500

    Refactorizacion de codigo, constantes de uso

---
+++ part1/unicafe/src/constants/details.js
@@ -0,0 +1,17 @@
+const course = 'Half Stack application development'
+const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 5
+  }
+]
+
+export { course, parts }
