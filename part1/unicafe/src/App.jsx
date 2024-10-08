3e117e032abba004a9ba916fc6ff6c95a5f0351b
Maicol Code <paucarjhon07@outlook.com>
2024-10-08 15:02:10 -0500

    Creacion de un solo objeto de uso en App

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,14 @@
 import { Content } from './components/Content'
 import { Header } from './components/Header'
 import { Total } from './components/Total'
-import { course, parts } from './constants/details'
+import { course } from './constants/details'
 
 function App() {
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
--- part1/unicafe/src/constants/details.js
+++ part1/unicafe/src/constants/details.js
@@ -1,17 +1,19 @@
-const course = 'Half Stack application development'
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 5
-  }
-]
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 5
+    }
+  ]
+}
 
-export { course, parts }
+export { course }
