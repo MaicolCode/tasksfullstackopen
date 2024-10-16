bc31681406887778115d6f0e8a77b6ed0d080a44
Maicol Code <paucarjhon07@outlook.com>
2024-10-16 00:16:12 -0500

    Se transfirio todo lo que contenia unicafe a course info en la carpeta App

---
+++ part1/courseinfo/app/src/components/Content.jsx
@@ -0,0 +1,11 @@
+import { Part } from './Part'
+
+export function Content({ parts }) {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
