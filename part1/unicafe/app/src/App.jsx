f96f5230f1bab63dae796e7c8046e25eae4f4a80
Maicol Code <paucarjhon07@outlook.com>
2024-10-16 22:05:31 -0500

    Nuevas funcionalidades para la presentacion de datos

--- part1/unicafe/app/src/App.jsx
+++ part1/unicafe/app/src/App.jsx
@@ -5,19 +5,36 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const totalReactions = good + neutral + bad
+  const average = ((good - bad) / totalReactions).toFixed(5)
+  const promedio = ((good / totalReactions) * 100).toFixed(5)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
       <h1>Give Feedback</h1>
       <div className='actions'>
-        <Button text='Good' />
-        <Button text='Neutral' />
-        <Button text='Bad' />
+        <Button actionClick={handleGood} text='Good' />
+        <Button actionClick={handleNeutral} text='Neutral' />
+        <Button actionClick={handleBad} text='Bad' />
       </div>
       <h1>Statistics</h1>
       <section className='statistics'>
         <span>Good: {good}</span>
         <span>Neutral: {neutral}</span>
         <span>Bad: {bad}</span>
+        <span>All: {totalReactions}</span>
+        <span>Average: {average}</span>
+        <span>Positive: {promedio} %</span>
       </section>
     </div>
   )
@@ -25,6 +42,6 @@ function App() {
 
 export default App
 
-function Button({ text }) {
-  return <button>{text}</button>
+function Button({ text, actionClick }) {
+  return <button onClick={actionClick}>{text}</button>
 }
