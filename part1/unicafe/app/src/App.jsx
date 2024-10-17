c77805dfc003fb90fd81341f54fa5f61b344f718
Maicol Code <paucarjhon07@outlook.com>
2024-10-16 21:46:21 -0500

    Estilos y forma a la pagina

--- part1/unicafe/app/src/App.jsx
+++ part1/unicafe/app/src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Give Feedback</h1>
+      <div className='actions'>
+        <Button text='Good' />
+        <Button text='Neutral' />
+        <Button text='Bad' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>Statistics</h1>
+      <section className='statistics'>
+        <span>Good: {good}</span>
+        <span>Neutral: {neutral}</span>
+        <span>Bad: {bad}</span>
+      </section>
+    </div>
   )
 }
 
 export default App
+
+function Button({ text }) {
+  return <button>{text}</button>
+}
